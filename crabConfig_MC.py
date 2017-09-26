2049b51ce975ca9799204dca639064c2eea3c7db
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2017-09-26 16:16:13 +0100

    preliminary CRAB3 config for MC run

---
+++ crabConfig_MC.py
@@ -0,0 +1,21 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'Ntupler'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'crab_mc_analysis.py'
+
+config.Data.inputDataset = '/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
+config.Data.outLFNDirBase = '/store/user/%s/v12.7/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+config.Data.outputDatasetTag = 'Ntupler'
+
+config.Site.storageSite = 'T2_PT_NCG_Lisbon'
+
