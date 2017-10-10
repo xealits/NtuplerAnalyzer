685e3746c862fd811f28d2eb154af8d6a4a6489a
Oleksii Toldaiev <alex.toldaiev@cern.ch>
2017-10-10 17:32:24 +0200

    TauMatch: penalties for simple SV, optimal direction tests
    Tried penalty on deviation from main direction and in pair SV finding.
    No luck.
    Tested some stuff in optimal direction, but didn't finish.
    
    Repeated Friday result, but there is a sign error somewhere -- fligth direction is flipped.
    
    Adding the notes with test commands.
    After all, Friday result + b cut at 0.9 cuts lepton+jets well.
    Tested in Medium and Tight taus -- looks ok.
    
    Did some calculations on optimal direction issues -- indeed could be significant effect.
    
    	new file:   notes
    	modified:   tauMatch_funcs.C

--- tauMatch_funcs.C
+++ tauMatch_funcs.C
@@ -997,9 +997,42 @@ double b_track_plane_intersections_average_perp_angle(
 // doesn't break with typos
 
 
+double max_track_bis_angle(
+	int which,
+	Float_t v1pt, Float_t v1eta, Float_t v1phi,
+	Float_t v2pt, Float_t v2eta, Float_t v2phi,
+	Float_t v3pt, Float_t v3eta, Float_t v3phi
+	)
+	{
+	TVector3 t1, t2, t3;
+	t1.SetPtEtaPhi(v1pt, v1eta, v1phi);
+	t2.SetPtEtaPhi(v2pt, v2eta, v2phi);
+	t3.SetPtEtaPhi(v3pt, v3eta, v3phi);
+
+	if (which == 0) // geometrical bis
+		{
+		t1.SetMag(1);
+		t2.SetMag(1);
+		t3.SetMag(1);
+		}
+
+	TVector3 bis = t1+t2+t3;
+
+	double an1 = t1.Angle(bis);
+	double an2 = t2.Angle(bis);
+	double an3 = t3.Angle(bis);
+
+	double max_an = (an1 > an2? an1 : an2);
+	if (an3>max_an) max_an = an3;
+
+	return max_an;
+	}
+
+
 // optimal directions
 // ok, it's late, let's try simple SV with this plane direction
 double optimal_directions_intersections_raw_SV(
+	int which, // for tests
 	Float_t b1x, Float_t b1y, Float_t b1z,
 	Float_t b2x, Float_t b2y, Float_t b2z,
 	Float_t b3x, Float_t b3y, Float_t b3z,
@@ -1043,6 +1076,7 @@ double optimal_directions_intersections_raw_SV(
 	//intr23.SetMag(1);
 	//intr31.SetMag(1);
 
+	// so this is the optimal direction
 	TVector3 average = intr12 + intr23 + intr31;
 	average.SetMag(1);
 	// got plane direction
@@ -1095,6 +1129,15 @@ double optimal_directions_intersections_raw_SV(
 	// weighted average with tracker errors
 	double x_average = (x12*syst12_weight + x23*syst23_weight + x31*syst31_weight) / (syst12_weight + syst23_weight + syst31_weight);
 	double x_deviation = (syst12_weight*pow(x12 - x_average, 2) + syst23_weight*pow(x23 - x_average, 2) + syst31_weight*pow(x31 - x_average, 2))/(2*(syst12_weight + syst23_weight + syst31_weight)/3);
+
+	switch (which)
+		{
+		// test optimal direction
+		case 1:
+			return average.Eta();
+		case 2:
+			return average.Angle(tau);
+		}
 
 	return x_average / sqrt(x_deviation);
 	}
@@ -1334,6 +1377,246 @@ double b_simple_SV_pt(
 
 	return x_average / sqrt(x_deviation);
 	}
+
+// penalty deviation from direction
+double b_simple_SV_pt_direction_penalty(
+	int which, // for tests etc
+	Float_t b1x, Float_t b1y, Float_t b1z,
+	Float_t b2x, Float_t b2y, Float_t b2z,
+	Float_t b3x, Float_t b3y, Float_t b3z,
+	Float_t v1pt, Float_t v1eta, Float_t v1phi,
+	Float_t v2pt, Float_t v2eta, Float_t v2phi,
+	Float_t v3pt, Float_t v3eta, Float_t v3phi
+	)
+        {
+	Float_t tracker_error = 0.2; // approximately systematic error on positions
+	// 0.0002 is not noticable
+	// 0.002 has slight effect
+	// cannot pass it as function parameter in root/cint/tformula!!!!!!
+
+	TVector3 b_vec1, b_vec2, b_vec3;
+	// B DIRECTION NEGATIVE
+	b_vec1.SetXYZ(-b1x, -b1y, -b1z);
+	b_vec2.SetXYZ(-b2x, -b2y, -b2z);
+	b_vec3.SetXYZ(-b3x, -b3y, -b3z);
+
+	TVector3 t1, t2, t3;
+	t1.SetPtEtaPhi(v1pt, v1eta, v1phi);
+	t2.SetPtEtaPhi(v2pt, v2eta, v2phi);
+	t3.SetPtEtaPhi(v3pt, v3eta, v3phi);
+
+	TVector3 t_sum = t1 + t2 + t3;
+	t_sum.SetMag(1);
+
+	// after establishing direction of tau
+	// tracks are only geometrical lines
+	t1.SetMag(1);
+	t2.SetMag(1);
+	t3.SetMag(1);
+
+	TVector3 b_long1 = t_sum * (b_vec1.Dot(t_sum));
+	TVector3 b_perp1 = b_vec1 - b_long1;
+
+	TVector3 b_long2 = t_sum * (b_vec2.Dot(t_sum));
+	TVector3 b_perp2 = b_vec2 - b_long2;
+
+	TVector3 b_long3 = t_sum * (b_vec3.Dot(t_sum));
+	TVector3 b_perp3 = b_vec3 - b_long3;
+
+	// in the perp plane find b long to tracks
+
+	// perpendicular parts of tracks
+	TVector3 t1_long = t_sum * (t1.Dot(t_sum));
+	TVector3 t1_perp = t1 - t1_long;
+	TVector3 t2_long = t_sum * (t2.Dot(t_sum));
+	TVector3 t2_perp = t2 - t2_long;
+	TVector3 t3_long = t_sum * (t3.Dot(t_sum));
+	TVector3 t3_perp = t3 - t3_long;
+
+	t1_perp.SetMag(1);
+	t2_perp.SetMag(1);
+	t3_perp.SetMag(1);
+
+	TVector3 perp_b_long1 = t1_perp * (b_perp1.Dot(t1_perp));
+	//TVector3 perp_b_perp1 = b_perp1 - perp_b_long1;
+
+	TVector3 perp_b_long2 = t2_perp * (b_perp2.Dot(t2_perp));
+	//TVector3 perp_b_perp2 = b_perp2 - perp_b_long2;
+
+	TVector3 perp_b_long3 = t3_perp * (b_perp3.Dot(t3_perp));
+	//TVector3 perp_b_perp3 = b_perp3 - perp_b_long3;
+
+
+	// the best point calculation
+	// approxiamtion, left for reference
+	TVector3 dV = t1 - t2;
+	TVector3 dB = perp_b_long1 - perp_b_long2;
+	double x12 = dV.Dot(dB) / dV.Mag2();
+	dV = t2 - t3;
+	dB = perp_b_long2 - perp_b_long3;
+	double x23 = dV.Dot(dB) / dV.Mag2();
+	dV = t3 - t1;
+	dB = perp_b_long3 - perp_b_long1;
+	double x31 = dV.Dot(dB) / dV.Mag2();
+
+	double pair_error1 = (perp_b_long1 + x12*t1 - (perp_b_long2 + x12*t2)).Mag();
+	double pair_error2 = (perp_b_long2 + x23*t2 - (perp_b_long3 + x23*t3)).Mag();
+	double pair_error3 = (perp_b_long3 + x31*t3 - (perp_b_long1 + x31*t1)).Mag();
+
+	// and systematic error of tracker
+	double syst12 = tracker_error / t1.Angle(t2); // technically / Sin (or Tan), but Sin = Angle with these angles
+	double syst23 = tracker_error / t2.Angle(t3); // technically / Sin (or Tan), but Sin = Angle with these angles
+	double syst31 = tracker_error / t3.Angle(t1); // technically / Sin (or Tan), but Sin = Angle with these angles
+	//double syst12 = sqrt(pow(pair_error1, 2) + pow(tracker_error / t1.Angle(t2), 2)); // technically / Sin (or Tan), but Sin = Angle with these angles
+	//double syst23 = sqrt(pow(pair_error2, 2) + pow(tracker_error / t2.Angle(t3), 2)); // technically / Sin (or Tan), but Sin = Angle with these angles
+	//double syst31 = sqrt(pow(pair_error3, 2) + pow(tracker_error / t3.Angle(t1), 2)); // technically / Sin (or Tan), but Sin = Angle with these angles
+	//double syst = pow(syst12, 2) + pow(syst23, 2) + pow(syst31, 2);
+	double syst12_weight = 1/syst12;
+	double syst23_weight = 1/syst23;
+	double syst31_weight = 1/syst31;
+
+	//double x_average = (x12 + x23 + x31) / 3;
+	//double x_deviation = pow(x12 - x_average, 2) + pow(x23 - x_average, 2) + pow(x31 - x_average, 2);
+	//double x_dev_syst = x_deviation + syst;
+
+	// weighted average with tracker errors (old, left for reference)
+	double x_average = (x12*syst12_weight + x23*syst23_weight + x31*syst31_weight) / (syst12_weight + syst23_weight + syst31_weight);
+	double x_deviation = (syst12_weight*pow(x12 - x_average, 2) + syst23_weight*pow(x23 - x_average, 2) + syst31_weight*pow(x31 - x_average, 2))/(2*(syst12_weight + syst23_weight + syst31_weight)/3);
+
+	// best point with full solution of linear equations -- it's not exactly possible! these are 3D vectors, they might not intersect!
+
+
+	// use mean best points
+	// and penalty convergence
+	TVector3 bestpoint_12 = 0.5 * (perp_b_long1 + x12*t1 + perp_b_long2 + x12*t2);
+	TVector3 bestpoint_23 = 0.5 * (perp_b_long2 + x23*t2 + perp_b_long3 + x23*t3);
+	TVector3 bestpoint_31 = 0.5 * (perp_b_long3 + x31*t3 + perp_b_long1 + x31*t1);
+
+	// average bp weighted with convergance
+	// deviations are raw to penalty convergence
+	syst12_weight = 1/pair_error1;
+	syst23_weight = 1/pair_error2;
+	syst31_weight = 1/pair_error3;
+	TVector3 bestpoint_average_weighted = (bestpoint_12*syst12_weight + bestpoint_23*syst23_weight + bestpoint_31*syst31_weight) * (1/(syst12_weight + syst23_weight + syst31_weight));
+	double bav_w = bestpoint_average_weighted.Mag();
+	TVector3 bestpoint_dev12_w = bestpoint_12 - bestpoint_average_weighted;
+	TVector3 bestpoint_dev23_w = bestpoint_23 - bestpoint_average_weighted;
+	TVector3 bestpoint_dev31_w = bestpoint_31 - bestpoint_average_weighted;
+	double bpdev_w = (pow(bestpoint_dev12_w.Mag(), 2) + pow(bestpoint_dev23_w.Mag(), 2) + pow(bestpoint_dev31_w.Mag(), 2))*(0.6666);
+
+	// let's try penalty divertion
+	bestpoint_average_weighted.SetMag(1);
+	TVector3 bpdev_w1_long = bestpoint_average_weighted * (bestpoint_dev12_w.Dot(bestpoint_average_weighted)); // dev on projection
+	TVector3 bpdev_w1_perp = bestpoint_dev12_w - bpdev_w1_long;
+	TVector3 bpdev_w2_long = bestpoint_average_weighted * (bestpoint_dev23_w.Dot(bestpoint_average_weighted));
+	TVector3 bpdev_w2_perp = bestpoint_dev23_w - bpdev_w2_long;
+	TVector3 bpdev_w3_long = bestpoint_average_weighted * (bestpoint_dev31_w.Dot(bestpoint_average_weighted));
+	TVector3 bpdev_w3_perp = bestpoint_dev31_w - bpdev_w3_long;
+
+	double bpdev_w_long = (pow(bpdev_w3_long.Mag(), 2) + pow(bpdev_w2_long.Mag(), 2) + pow(bpdev_w1_long.Mag(), 2))*(0.6666);
+	double bpdev_w_perp = (pow(bpdev_w3_perp.Mag(), 2) + pow(bpdev_w2_perp.Mag(), 2) + pow(bpdev_w1_perp.Mag(), 2))*(0.6666);
+
+
+	//TVector3 bestpoint_12 = perp_b_long1 + x12*t1;
+	//TVector3 bestpoint_23 = perp_b_long2 + x23*t2;
+	//TVector3 bestpoint_31 = perp_b_long3 + x31*t3;
+	TVector3 bestpoint_average = (bestpoint_12 + bestpoint_23 + bestpoint_31) * (0.3333);
+	TVector3 bestpoint_dev12 = bestpoint_12 - bestpoint_average;
+	TVector3 bestpoint_dev23 = bestpoint_23 - bestpoint_average;
+	TVector3 bestpoint_dev31 = bestpoint_31 - bestpoint_average;
+
+	// now project them on t_sum, find perp parts, find deviation along perp average
+	// t_sum MIGHT BE WRONG
+	TVector3 bpav_long = t_sum * (bestpoint_average.Dot(t_sum)); // 1) best point along the direction
+	double bpav_proj = bpav_long.Mag();
+	TVector3 bpav_perp = bestpoint_average - bpav_long;          // 2) best point perpendicular to direction, divertion from direction
+	double bpav_divert = bpav_perp.Mag();
+	// now deviations are needed
+	bpav_perp.SetMag(1);
+
+	TVector3 bpdev1_long = t_sum * (bestpoint_dev12.Dot(t_sum)); // dev on projection
+	TVector3 bpdev1_perp = bpav_perp * (bpav_perp.Dot(bestpoint_dev12 - bpdev1_long)); // dev on divertion
+	TVector3 bpdev2_long = t_sum * (bestpoint_dev23.Dot(t_sum));
+	TVector3 bpdev2_perp = bpav_perp * (bpav_perp.Dot(bestpoint_dev23 - bpdev2_long));
+	TVector3 bpdev3_long = t_sum * (bestpoint_dev31.Dot(t_sum));
+	TVector3 bpdev3_perp = bpav_perp * (bpav_perp.Dot(bestpoint_dev31 - bpdev3_long));
+
+	// dev-s of projection and divertion
+	//double bpdev_proj = bp
+	//double bpdev_proj = (syst12_weight*pow(bpdev1_long.Mag(), 2) + syst23_weight*pow(bpdev2_long.Mag(), 2) + syst31_weight*pow(bpdev3_long.Mag(), 2))/(2*(syst12_weight + syst23_weight + syst31_weight)/3);
+	//double bpdev_divert = (syst12_weight*pow(bpdev1_perp.Mag(), 2) + syst23_weight*pow(bpdev2_perp.Mag(), 2) + syst31_weight*pow(bpdev3_perp.Mag(), 2))/(2*(syst12_weight + syst23_weight + syst31_weight)/3);
+	// no weights
+	double bpdev_proj   = (pow(bpdev1_long.Mag(), 2) + pow(bpdev2_long.Mag(), 2) + pow(bpdev3_long.Mag(), 2))*(0.6666);
+	double bpdev_divert = (pow(bpdev1_perp.Mag(), 2) + pow(bpdev2_perp.Mag(), 2) + pow(bpdev3_perp.Mag(), 2))*(0.6666);
+
+	switch (which)
+		{
+		// test finding paired best points
+		case 1:
+			// test best point 12
+			return pair_error1;
+		case 2:
+			// test best point 23
+			return pair_error2;
+		case 3:
+			// test best point 31
+			return pair_error3;
+
+		// test average best point etc
+		case 4:
+			return bpav_proj;
+		case 5:
+			return bpav_divert;
+		case 6:
+			return bpdev_proj;
+		case 7:
+			return bpdev_divert;
+
+		// old calculation results, for reference
+		case 8:
+			return x_average;
+		case 9:
+			return x_deviation;
+		case 10:
+			if (x_deviation > 0)
+				return x_average / sqrt(x_deviation);
+			else
+				return - x_average / sqrt(x_deviation);
+
+		// different sign parameters
+		case 11: // no penalties
+			return (bpav_proj/bpdev_proj);
+		case 12: // penalty divertion
+			return (bpav_proj/bpdev_proj) / (1 + bpav_divert/bpdev_divert);
+		case 13: // penalty convergence
+			return (bpav_proj/bpdev_proj) / (1 + pair_error1 + pair_error2 + pair_error3);
+		// deviations and penalty values
+		case 14:
+			return bpav_divert/bpdev_divert;
+		case 15:
+			return pair_error1 + pair_error2 + pair_error3;
+
+		// convergance-weighted average, but raw dev to penalty convergence
+		case 16:
+			return bav_w;
+		case 17:
+			return bpdev_w;
+		case 18:
+			return bav_w / bpdev_w;
+		case 19:
+			return bpdev_w_long;
+		case 20:
+			return bpdev_w_perp;
+		case 21:
+			return (bav_w / bpdev_w_long) / (1 + bpdev_w_perp);
+		}
+
+	//return x_average / sqrt(x_deviation);
+	return (bpav_proj/bpdev_proj) / (1 + bpav_divert/bpdev_divert + pair_error1 + pair_error2 + pair_error3);
+	}
+
+
+
 
 
 
