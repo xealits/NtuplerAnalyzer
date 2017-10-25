481f3b3a37f66c806292432b2f6e7b3d32a4269c
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2017-10-25 15:09:43 +0100

    added the norm and stack plot scripts

---
+++ jobsums/mc_norm_to_data/normalize.py
@@ -0,0 +1,245 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+parser = argparse.ArgumentParser(
+    formatter_class = argparse.RawDescriptionHelpFormatter,
+    description = "normalize histograms to luminosity",
+    #epilog = "Example:\n$ python job_submit.py ttbarleps80_eventSelection jobing/my_runAnalysis_cfg_NEWSUBMIT.templ.py bin/ttbar-leptons-80X/analysis/dsets_testing_noHLT_TTbar.json test/tests/outdir_test_v11_ttbar_v8.40/"
+    )
+
+parser.add_argument('-l', '--lumi', type=float, default=35900, help="luminosity in pb")
+
+args = parser.parse_args()
+
+logging.info("import ROOT")
+
+import ROOT
+from ROOT import TFile
+
+
+W_lep_br = 0.108;
+W_qar_br = 0.676;
+
+W_lep_br2 = W_lep_br*W_lep_br;
+W_qar_br2 = W_qar_br*W_qar_br;
+
+br_tau_electron = 0.1785;
+br_tau_muon     = 0.1736;
+br_tau_lepton   = br_tau_electron + br_tau_muon;
+br_tau_hadronic = 1 - br_tau_lepton;
+
+ttbar_xsec = 831.76;
+
+xsecs = {
+"MC2016_Summer16_DYJetsToLL_10to50_amcatnlo": 18610,
+"MC2016_Summer16_DYJetsToLL_10to50_amcatnlo_": 18610,
+"MC2016_Summer16_DYJetsToLL_50toInf_madgraph": 5765.4, # but McM shows 4970
+#"MC2016_Summer16_DYJetsToLL_50toInf_madgraph_": 6025.2, // FIXME: the DY x-section was updated by about 5%
+"MC2016_Summer16_DYJetsToLL_50toInf_madgraph_": 4970, # FIXME: the DY x-section was updated by about 5%
+"MC2016_Summer16_QCD_HT-100-200":  27540000,
+"MC2016_Summer16_QCD_HT-100-200_":  27540000,
+"MC2016_Summer16_QCD_HT-1000-1500":  1206,
+"MC2016_Summer16_QCD_HT-1000-1500_":  1206,
+"MC2016_Summer16_QCD_HT-1500-2000":  120.4,
+"MC2016_Summer16_QCD_HT-1500-2000_":  120.4,
+"MC2016_Summer16_QCD_HT-200-300":  1717000,
+"MC2016_Summer16_QCD_HT-200-300_":  1717000,
+"MC2016_Summer16_QCD_HT-2000-Inf":  25.25,
+"MC2016_Summer16_QCD_HT-2000-Inf_":  25.25,
+"MC2016_Summer16_QCD_HT-300-500":  351300,
+"MC2016_Summer16_QCD_HT-300-500_":  351300,
+"MC2016_Summer16_QCD_HT-500-700":  31630,
+"MC2016_Summer16_QCD_HT-500-700_":  31630,
+"MC2016_Summer16_QCD_HT-700-1000":  6802,
+"MC2016_Summer16_QCD_HT-700-1000_":  6802,
+"MC2016_Summer16_SingleT_tW_5FS_powheg_":    35.6,
+"MC2016_Summer16_SingleT_tW_5FS_powheg":    35.6,
+"MC2016_Summer16_SingleTbar_tW_5FS_powheg_": 35.6,
+"MC2016_Summer16_SingleTbar_tW_5FS_powheg": 35.6,
+ "MC2016_Summer16_TTJets_powheg"      : ttbar_xsec ,
+ "MC2016_Summer16_TTJets_powheg_1"      : ttbar_xsec ,
+ "MC2016_Summer16_TTJets_powheg_2"      : ttbar_xsec ,
+ "MC2016_Summer16_TTJets_powheg_3"      : ttbar_xsec ,
+ "MC2016_Summer16_TTJets_powheg_4"      : ttbar_xsec ,
+ "MC2016_Summer16_TTJets_powheg_5"      : ttbar_xsec ,
+ "MC2016_Summer16_TTJets_powheg_2_aelmtuu" :  ttbar_xsec * W_lep_br2 * 2 * br_tau_lepton ,
+ "MC2016_Summer16_TTJets_powheg_2_elmu"    :  ttbar_xsec * W_lep_br2 * 2 ,
+ "MC2016_Summer16_TTJets_powheg_2_other"   :  ttbar_xsec * (1 - 2*W_lep_br2 - 2*W_lep_br2*br_tau_lepton) ,
+ "MC2016_Summer16_TTJets_powheg_aattuu"      : ttbar_xsec * W_lep_br2 ,
+ "MC2016_Summer16_TTJets_powheg_aeltu"       : ttbar_xsec * W_lep_br2 * 2 ,
+ "MC2016_Summer16_TTJets_powheg_amtuu"       : ttbar_xsec * W_lep_br2 * 2 ,
+ "MC2016_Summer16_TTJets_powheg_aqtu"        : ttbar_xsec * W_lep_br*W_qar_br * 2,
+ "MC2016_Summer16_TTJets_powheg_aaelmttuuu":  ttbar_xsec * W_lep_br2 *2 * br_tau_muon     * br_tau_electron,
+ "MC2016_Summer16_TTJets_powheg_aaeellttuu":  ttbar_xsec * W_lep_br2    * br_tau_electron * br_tau_electron ,
+ "MC2016_Summer16_TTJets_powheg_aaehlttuu" :  ttbar_xsec * W_lep_br2 *2 * br_tau_hadronic * br_tau_electron ,
+ "MC2016_Summer16_TTJets_powheg_aammttuuuu":  ttbar_xsec * W_lep_br2    * br_tau_muon     * br_tau_muon ,
+ "MC2016_Summer16_TTJets_powheg_aahmttuuu" :  ttbar_xsec * W_lep_br2 *2 * br_tau_hadronic * br_tau_muon ,
+ "MC2016_Summer16_TTJets_powheg_aahhttuu"  :  ttbar_xsec * W_lep_br2    * br_tau_hadronic * br_tau_hadronic,
+ "MC2016_Summer16_TTJets_powheg_aelmtuu"   :  ttbar_xsec * W_lep_br2         * 2 * br_tau_lepton ,
+ "MC2016_Summer16_TTJets_powheg_aeelltu"   :  ttbar_xsec * W_lep_br2         * 2 * br_tau_electron ,
+ "MC2016_Summer16_TTJets_powheg_ammtuuu"   :  ttbar_xsec * W_lep_br2         * 2 * br_tau_muon ,
+ "MC2016_Summer16_TTJets_powheg_aelqtu"    :  ttbar_xsec * W_lep_br*W_qar_br * 2 * br_tau_electron ,
+ "MC2016_Summer16_TTJets_powheg_amqtuu"    :  ttbar_xsec * W_lep_br*W_qar_br * 2 * br_tau_muon ,
+ "MC2016_Summer16_TTJets_powheg_aehltu"     : ttbar_xsec * W_lep_br2 * 2         * br_tau_hadronic,
+ "MC2016_Summer16_TTJets_powheg_ahmtuu"     : ttbar_xsec * W_lep_br2 * 2         * br_tau_hadronic,
+ "MC2016_Summer16_TTJets_powheg_ahqtu"      : ttbar_xsec * W_lep_br*W_qar_br * 2 * br_tau_hadronic,
+ "MC2016_Summer16_TTJets_powheg_eell"      : ttbar_xsec * W_lep_br2 ,
+ "MC2016_Summer16_TTJets_powheg_elmu"      : ttbar_xsec * W_lep_br2 * 2 ,
+ "MC2016_Summer16_TTJets_powheg_elq"       : ttbar_xsec * W_lep_br*W_qar_br * 2,
+ "MC2016_Summer16_TTJets_powheg_mmuu"      : ttbar_xsec * W_lep_br2 ,
+ "MC2016_Summer16_TTJets_powheg_mqu"       : ttbar_xsec * W_lep_br*W_qar_br * 2,
+ "MC2016_Summer16_TTJets_powheg_qq"        : ttbar_xsec * W_qar_br2 ,
+ "MC2016_Summer16_TTJets_powheg_aattuu"      : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aeltu"       : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_amtuu"       : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aqtu"        : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aaelmttuuu":  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aaeellttuu":  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aaehlttuu" :  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aammttuuuu":  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aahmttuuu" :  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aahhttuu"  :  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aelmtuu"   :  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aeelltu"   :  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_ammtuuu"   :  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aelqtu"    :  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_amqtuu"    :  ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aehltu"     : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_ahmtuu"     : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_ahqtu"      : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_eell"      : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_elmu"      : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_elq"       : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_mmuu"      : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_mqu"       : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_qq"        : ttbar_xsec,
+ "MC2016_Summer16_TTJets_powheg_aattuu"      : ttbar_xsec * W_lep_br2 ,
+ "MC2016_Summer16_TTJets_powheg_aeltu"       : ttbar_xsec * W_lep_br2 * 2 ,
+ "MC2016_Summer16_TTJets_powheg_amtuu"       : ttbar_xsec * W_lep_br2 * 2 ,
+ "MC2016_Summer16_TTJets_powheg_aqtu"        : ttbar_xsec * W_lep_br*W_qar_br ,
+ "MC2016_Summer16_TTJets_powheg_eell"        : ttbar_xsec * W_lep_br2 ,
+ "MC2016_Summer16_TTJets_powheg_elmu"        : ttbar_xsec * W_lep_br2 * 2 ,
+ "MC2016_Summer16_TTJets_powheg_elq"         : ttbar_xsec * W_lep_br*W_qar_br ,
+ "MC2016_Summer16_TTJets_powheg_mmuu"        : ttbar_xsec * W_lep_br2 ,
+ "MC2016_Summer16_TTJets_powheg_mqu"         : ttbar_xsec * W_lep_br*W_qar_br ,
+ "MC2016_Summer16_TTJets_powheg_qq"          : ttbar_xsec * W_qar_br2 ,
+"MC2016_Summer16_W1Jets_madgraph_perc": 8921.44,
+"MC2016_Summer16_W1Jets_madgraph": 0.65 *  9493,
+"MC2016_Summer16_W1Jets_madgraph_": 9493,
+"MC2016_Summer16_W2Jets_madgraph_perc": 2838.64,
+"MC2016_Summer16_W2Jets_madgraph": 0.65 *  3120,
+"MC2016_Summer16_W2Jets_madgraph_": 3120,
+"MC2016_Summer16_W3Jets_madgraph_perc":  861.73,
+"MC2016_Summer16_W3Jets_madgraph": 0.65 *  942.3,
+"MC2016_Summer16_W3Jets_madgraph_": 942.3,
+"MC2016_Summer16_W4Jets_madgraph_perc":  354.83,
+"MC2016_Summer16_W4Jets_madgraph": 0.65 *  524.2,
+"MC2016_Summer16_W4Jets_madgraph_": 524.2,
+"MC2016_Summer16_WJets_amcatnlo": 61526.7 - 9493 - 3120 - 942.3 - 524.2,
+"MC2016_Summer16_WJets_amcatnlo_full": 61526.7,
+"MC2016_Summer16_WJets_amcatnlo_": 61526.7,
+"MC2016_Summer16_WJets_madgraph_075": 50690 - 9493 - 3120 - 942.3 - 524.2,
+"MC2016_Summer16_WJets_madgraph_perc": 37713.4 ,
+"MC2016_Summer16_WJets_madgraph":  0.65 * 61526.7, # 50690 - 9493 - 3120 - 942.3 - 524.2},
+ "MC2016_Summer16_WWTo2L2Nu_powheg"               :  12.178  ,
+ "MC2016_Summer16_WWToLNuQQ_powheg"               :  49.997  ,
+ "MC2016_Summer16_WZTo1L1Nu2Q_amcatnlo_madspin"   : 10.71  ,
+ "MC2016_Summer16_WZTo1L3Nu_amcatnlo_madspin"     : 3.033  ,
+ "MC2016_Summer16_WZTo2L2Q_amcatnlo_madspin"      : 5.595  ,
+ "MC2016_Summer16_WZTo3LNu_powheg"                : 4.42965  ,
+ "MC2016_Summer16_ZZTo2L2Nu_powheg"               : 0.564  ,
+ "MC2016_Summer16_ZZTo2L2Nu_powheg"               : 1.256  ,
+ "MC2016_Summer16_ZZTo2L2Q_amcatnlo_madspin"      : 3.22   ,
+"MC2016_Summer16_schannel_4FS_leptonicDecays_amcatnlo_": 10.11, #3.36},
+"MC2016_Summer16_schannel_4FS_leptonicDecays_amcatnlo": 10.11, #3.36},
+"MC2016_Summer16_tchannel_antitop_4f_leptonicDecays_powheg_": 80.95, #70.69/2},
+"MC2016_Summer16_tchannel_antitop_4f_leptonicDecays_powheg": 80.95, #70.69/2},
+"MC2016_Summer16_tchannel_top_4f_leptonicDecays_powheg_": 136.02, #70.69/2},
+"MC2016_Summer16_tchannel_top_4f_leptonicDecays_powheg": 136.02, #70.69/2},
+}
+
+files = [
+"MC2016_Summer16_DYJetsToLL_10to50_amcatnlo",
+"MC2016_Summer16_DYJetsToLL_50toInf_madgraph",
+"MC2016_Summer16_QCD_HT-100-200",
+"MC2016_Summer16_QCD_HT-1000-1500",
+"MC2016_Summer16_QCD_HT-1500-2000",
+"MC2016_Summer16_QCD_HT-200-300",
+"MC2016_Summer16_QCD_HT-2000-Inf",
+"MC2016_Summer16_QCD_HT-300-500",
+"MC2016_Summer16_QCD_HT-500-700",
+"MC2016_Summer16_QCD_HT-700-1000",
+"MC2016_Summer16_SingleT_tW_5FS_powheg",
+"MC2016_Summer16_SingleTbar_tW_5FS_powheg",
+"MC2016_Summer16_TTJets_powheg",
+"MC2016_Summer16_W1Jets_madgraph",
+"MC2016_Summer16_W2Jets_madgraph",
+"MC2016_Summer16_W3Jets_madgraph",
+"MC2016_Summer16_W4Jets_madgraph",
+"MC2016_Summer16_WJets_amcatnlo",
+"MC2016_Summer16_WJets_madgraph",
+"MC2016_Summer16_WWTo2L2Nu_powheg",
+"MC2016_Summer16_WWToLNuQQ_powheg",
+"MC2016_Summer16_WZTo1L1Nu2Q_amcatnlo_madspin",
+"MC2016_Summer16_WZTo1L3Nu_amcatnlo_madspin",
+"MC2016_Summer16_WZTo2L2Q_amcatnlo_madspin",
+"MC2016_Summer16_WZTo3LNu_powheg",
+"MC2016_Summer16_ZZTo2L2Nu_powheg",
+"MC2016_Summer16_ZZTo2L2Q_amcatnlo_madspin",
+"MC2016_Summer16_schannel_4FS_leptonicDecays_amcatnlo",
+"MC2016_Summer16_tchannel_antitop_4f_leptonicDecays_powheg",
+"MC2016_Summer16_tchannel_top_4f_leptonicDecays_powheg"]
+
+
+
+for fname in files:
+    f = TFile(fname + '.root', "UPDATE")
+    #weight_counter = f.Get('weight_counter')
+    #weight_counter->GetBinContent(2)
+    #scale = xsecs[fname] / weight_counter.GetBinContent(2)
+    # norm to lumi mu+tau
+    scale = args.lumi
+
+    #logging.info("%s %f" % (fname, scale))
+
+    for proc in list(f.GetListOfKeys()):
+        process = proc.GetName()
+        if process == 'weight_counter' or process == 'events_counter':
+            continue
+
+        #try:
+        for chan in list(proc.ReadObj().GetListOfKeys()):
+            nick = chan.GetName()
+            for sys in list(chan.ReadObj().GetListOfKeys()):
+                sys_name = sys.GetName()
+                for histo_key in list(sys.ReadObj().GetListOfKeys()):
+                    h = histo_key.ReadObj()
+                    #h.Print()
+                    #h.Sumw2() # to keep errors correctly
+                    # it still complains that the structure is there
+                    if process == 'mu_sel' and sys_name == 'NOMINAL' and histo_key.GetName() == '_'.join([process, sys_name, nick, 'Mt_lep_met']):
+                        integral_init = h.Integral()
+                        err_init = 0.
+                        for i in range(h.GetSize()):
+                            err_init += h.GetBinError(i)
+
+                    h.Sumw2(ROOT.kTRUE) # to correctly save the errors after scaling
+                    # it will raise Warnin for histograms which already have this -- have to filter it offline
+                    h.Scale(scale)
+                    #h.Print()
+                    if process == 'mu_sel' and sys_name == 'NOMINAL' and histo_key.GetName() == '_'.join([process, sys_name, nick, 'Mt_lep_met']):
+                        err_scaled = 0.
+                        for i in range(h.GetSize()):
+                            err_scaled += h.GetBinError(i)
+                        logging.debug("%s\t%s %10.f %5.5f %5.5f %3.3f %3.3f" % (fname, nick, scale, err_init/integral_init if integral_init > 0 else err_init, err_scaled/h.Integral() if h.Integral() > 0 else err_scaled, integral_init, h.Integral()))
+
+        #except AttributeError as e:
+            #continue #print e
+            #print e
+
+    f.Write()
+    f.Close()
+
+
---
+++ jobsums/stack_plot.py
@@ -0,0 +1,169 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+parser = argparse.ArgumentParser(
+    formatter_class = argparse.RawDescriptionHelpFormatter,
+    description = "stack histos",
+    #epilog = "Example:\n$ python job_submit.py ttbarleps80_eventSelection jobing/my_runAnalysis_cfg_NEWSUBMIT.templ.py bin/ttbar-leptons-80X/analysis/dsets_testing_noHLT_TTbar.json test/tests/outdir_test_v11_ttbar_v8.40/"
+    )
+
+parser.add_argument("mc_file",    help="MC file name")
+parser.add_argument("data_file",  help="Data file name")
+parser.add_argument("-c", "--channel",  type=str, default='mu_sel', help="selection of events")
+parser.add_argument("-s", "--systematic",  type=str, default='NOMINAL', help="systematic variation")
+parser.add_argument("-d", "--distr",  type=str, default='Mt_lep_met', help="recorded distribution")
+parser.add_argument("-x", "--shape",  type=str, default='', help="selection for shape distributions of wjets and dy")
+
+args = parser.parse_args()
+
+logging.info("import ROOT")
+
+import ROOT
+from ROOT import TFile, THStack, TLegend, kGreen, kYellow, kOrange, kViolet, kAzure, kWhite, kGray, kRed, kCyan
+
+#channel = "mu_presel"
+#channel = "mu_sel"
+#channel = "mu_lj"
+#channel = "mu_lj_out"
+channel = args.channel
+
+#sys_name = "NOMINAL"
+sys_name = args.systematic
+#distr_name = 'mu_presel_NOMINAL_wjets_Mt_lep_met'
+
+#distr_name = 'Mt_lep_met'
+#'met'
+#'Mt_lep_met'
+##'Mt_lep_met_d'
+#'Mt_tau_met'
+#'njets'
+#'nbjets'
+#'dijet_trijet_mass'
+#distr_name = 'dijet_trijet_mass'
+distr_name = args.distr
+
+fdata = TFile(args.data_file)
+
+# only nominal in data
+#histo_data = fdata.Get(channel + '/data/' + sys_name + '/' + '_'.join([channel, sys_name, 'data', distr_name]))
+histo_data = fdata.Get(channel + '/data/NOMINAL/' + '_'.join([channel, 'NOMINAL', 'data', distr_name]))
+histo_data.Print()
+logging.debug("data: %f" % histo_data.Integral())
+
+f = TFile(args.mc_file)
+
+hs = THStack("mc_stack", "mc_stack")
+
+nick_colour = {
+"data": kWhite,
+"dy": kGray,
+"wjets": kRed+1,
+"dibosons": kCyan,
+"singletop": kAzure,
+
+"tt_jj": kGreen+4,
+"tt_lj": kGreen+3,
+"tt_em": kYellow-7,
+"tt_{em}": kYellow-7,
+"tt_ee": kAzure-9,
+"tt_mm": kGreen-9,
+"tt_eltau": kOrange+2,
+"tt_mutau": kOrange+1,
+"tt_{l\\tau-l}": kOrange+3,
+"tt_other": kCyan-5,
+
+"qcd": kViolet,
+}
+
+leg = TLegend(0.7, 0.7, 0.89, 0.89)
+
+used_histos = []
+
+for chan in list(f.GetListOfKeys()):
+    if chan.GetName() == 'weight_counter' or chan.GetName() == 'events_counter':
+        continue
+    if chan.GetName() != channel and chan.GetName() != args.shape:
+        continue
+
+    processes_keys = list(chan.ReadObj().GetListOfKeys())
+    sorted_pkeys = sorted(processes_keys, key=lambda pkey: pkey.GetName() if pkey.GetName() not in ('qcd') else 'z_' + pkey.GetName())
+    for process in sorted_pkeys:
+        nick = process.GetName()
+        logging.info(nick)
+
+        if nick == 'data':
+            continue
+
+        # if the channel with shapes for dy and wjets are given
+        # don't use other channels
+        if args.shape and nick in ('dy', 'wjets') and chan.GetName() != args.shape:
+            continue
+
+        for sys in list(process.ReadObj().GetListOfKeys()):
+            if 'TOPPT' in sys_name:
+                fixed_sys_name = sys_name if 'tt' in nick else 'NOMINAL'
+            if fixed_sys_name != sys.GetName():
+                continue
+
+            try:
+                for histo_key in list(sys.ReadObj().GetListOfKeys()):
+                    # rename for given shapes of dy and wjets
+                    if args.shape and nick in ('dy', 'wjets'):
+                        histo_name = '_'.join([args.shape, fixed_sys_name, nick, distr_name])
+                    else:
+                        histo_name = '_'.join([channel, fixed_sys_name, nick, distr_name])
+
+                    if histo_key.GetName() != histo_name:
+                        continue
+
+                    histo = histo_key.ReadObj()
+                    logging.info("%s   %s   %x = %f %f" % (histo_name, histo_key.GetName(), histo_key.GetName() == '_'.join([channel, fixed_sys_name, nick, distr_name]), histo.GetEntries(), histo.Integral()))
+
+                    col = nick_colour[nick]
+                    histo.SetFillColor( col );
+                    histo.SetMarkerStyle(20);
+                    histo.SetLineStyle(0);
+                    histo.SetMarkerColor(col);
+                    used_histos.append(histo) # hopefully root wont screw this up
+                    hs.Add(histo, "HIST")
+                    leg.AddEntry(histo, nick, "F")
+            except Exception as e:
+                print "failed", nick, e.__class__, e.__doc__
+
+# get the sum of histograms
+histos = hs.GetHists() # TList
+hs_sum1 = histos[0].Clone() #TH1F("sum","sum of histograms",100,-4,4);
+hs_sum1.SetName('mc_sum1')
+
+for h in histos[1:]:
+    hs_sum1.Add(h)
+
+hs_sum1.SetFillStyle(3004);
+hs_sum1.SetFillColor(1);
+hs_sum1.SetMarkerStyle(1)
+hs_sum1.SetMarkerColor(0)
+
+hs_sum2 = used_histos[0].Clone() #TH1F("sum","sum of histograms",100,-4,4);
+hs_sum2.SetName('mc_sum2')
+
+for h in used_histos[1:]:
+    hs_sum2.Add(h)
+
+hs_sum2.SetFillStyle(3004);
+hs_sum2.SetFillColor(1);
+hs_sum2.SetMarkerStyle(1)
+hs_sum2.SetMarkerColor(0)
+
+fout = TFile(args.mc_file.split('.root')[0] + '_%s_%s_%s.root' % (distr_name, channel, sys_name), 'RECREATE')
+fout.cd()
+histo_data.Write() #'data')
+hs.Write()
+hs_sum1.Write()
+hs_sum2.Write()
+leg.Write('leg')
+fout.Write()
+fout.Close()
+
+
