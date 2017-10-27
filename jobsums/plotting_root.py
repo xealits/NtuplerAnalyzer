3a90dac1aead80d7b54980b5617464196b47a6db
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2017-10-27 16:41:23 +0100

    upgraded plotting scripts

---
+++ jobsums/plotting_root.py
@@ -0,0 +1,45 @@
+from ROOT import THStack, TLegend, kGreen, kYellow, kOrange, kViolet, kAzure, kWhite, kGray, kRed, kCyan
+
+nick_colour = {
+"data": kWhite,
+"dy": kGray,
+"dy_other": kGray,
+"dy_tautau": kGray+2,
+"wjets": kRed+1,
+"dibosons": kCyan,
+"singletop": kAzure,
+"s_top_eltau": kAzure,
+"s_top_lj": kAzure+1,
+"s_top_other": kAzure+2,
+
+"tt_taultauh" : kCyan-3,
+"tt_jj": kGreen+4,
+"tt_lj": kGreen+3,
+"tt_em": kYellow-7,
+"tt_{em}": kYellow-7,
+"tt_ee": kAzure-9,
+"tt_mm": kGreen-9,
+"tt_eltau": kOrange+2,
+"tt_mutau": kOrange+1,
+"tt_{l\\tau-l}": kOrange+3,
+"tt_other": kCyan-5,
+
+"qcd": kViolet,
+}
+
+
+def stack_n_legend(used_histos):
+    # build Stach and legend
+    hs = THStack("mc_stack", "mc_stack")
+    leg = TLegend(0.7, 0.7, 0.89, 0.89)
+    for histo, nick in used_histos:
+        col = nick_colour[nick]
+        histo.SetFillColor( col );
+        histo.SetMarkerStyle(20);
+        histo.SetLineStyle(0);
+        histo.SetMarkerColor(col);
+        #used_histos.append(histo) # hopefully root wont screw this up
+        hs.Add(histo, "HIST")
+        leg.AddEntry(histo, nick, "F")
+    return hs, leg
+
--- jobsums/stack_plot.py
+++ jobsums/stack_plot.py
@@ -15,6 +15,8 @@ parser.add_argument("-c", "--channel",  type=str, default='mu_sel', help="select
 parser.add_argument("-s", "--systematic",  type=str, default='NOMINAL', help="systematic variation")
 parser.add_argument("-d", "--distr",  type=str, default='Mt_lep_met', help="recorded distribution")
 parser.add_argument("-x", "--shape",  type=str, default='', help="selection for shape distributions of wjets and dy")
+parser.add_argument("-p", "--plot",  action = "store_true", help="don't save the root file, plot stacked histogram in png")
+parser.add_argument("-r", "--ratio", action = "store_true", help="don't save the root file, make ratio plot (in addition to stack or alone)")
 
 args = parser.parse_args()
 
@@ -22,6 +24,7 @@ logging.info("import ROOT")
 
 import ROOT
 from ROOT import TFile, THStack, TLegend, kGreen, kYellow, kOrange, kViolet, kAzure, kWhite, kGray, kRed, kCyan
+import plotting_root
 
 #channel = "mu_presel"
 #channel = "mu_sel"
@@ -52,94 +55,86 @@ histo_data = fdata.Get(channel + '/data/NOMINAL/' + '_'.join([channel, 'data', '
 histo_data.Print()
 logging.debug("data: %f" % histo_data.Integral())
 
+
+def get_histos(infile, channel, shape_channel, sys_name, distr_name):
+    """get_histos(infile)
+
+    the file contains usual structure
+    <channels>/<processes>/<systematics>/<histograms>
+
+    returns [(histo, nick)]
+    """
+    used_histos = [] # (histo, nick)
+
+    for chan in list(f.GetListOfKeys()):
+       if chan.GetName() == 'weight_counter' or chan.GetName() == 'events_counter':
+           continue
+       if chan.GetName() != channel and chan.GetName() != shape_channel:
+           continue
+
+       processes_keys = list(chan.ReadObj().GetListOfKeys())
+       sorted_pkeys = sorted(processes_keys, key=lambda pkey: pkey.GetName() if pkey.GetName() not in ('qcd') else 'z_' + pkey.GetName())
+       for process in sorted_pkeys:
+           nick = process.GetName()
+           logging.info(nick)
+
+           if nick == 'data':
+               continue
+
+           # if the channel with shapes for dy and wjets are given
+           # don't use other channels
+           if shape_channel and nick in ('dy', 'wjets') and chan.GetName() != shape_channel:
+               continue
+
+           for sys in list(process.ReadObj().GetListOfKeys()):
+               fixed_sys_name = sys_name
+               if 'TOPPT' in sys_name:
+                   fixed_sys_name = sys_name if 'tt' in nick else 'NOMINAL'
+               if fixed_sys_name != sys.GetName():
+                   continue
+
+               try:
+                   for histo_key in list(sys.ReadObj().GetListOfKeys()):
+                       # rename for given shapes of dy and wjets
+                       if shape_channel and nick in ('dy', 'wjets'):
+                           histo_name = '_'.join([shape_channel, nick, fixed_sys_name, distr_name])
+                       else:
+                           histo_name = '_'.join([channel, nick, fixed_sys_name, distr_name])
+
+                       if histo_key.GetName() != histo_name:
+                           continue
+
+                       histo = histo_key.ReadObj()
+                       logging.info("%s   %s   %x = %f %f" % (histo_name, histo_key.GetName(), histo_key.GetName() == '_'.join([channel, nick, fixed_sys_name, distr_name]), histo.GetEntries(), histo.Integral()))
+
+                       #col = nick_colour[nick]
+                       #histo.SetFillColor( col );
+                       #histo.SetMarkerStyle(20);
+                       #histo.SetLineStyle(0);
+                       #histo.SetMarkerColor(col);
+                       used_histos.append((histo, nick)) # hopefully root wont screw this up
+                       #hs.Add(histo, "HIST")
+                       #leg.AddEntry(histo, nick, "F")
+
+               except Exception as e:
+                   print "failed", nick, e.__class__, e.__doc__
+
+    return used_histos
+
 f = TFile(args.mc_file)
+used_histos = get_histos(f, channel, args.shape, sys_name, distr_name)
 
-hs = THStack("mc_stack", "mc_stack")
+# get MC stack and legend for it
+hs, leg = plotting_root.stack_n_legend(used_histos)
 
-nick_colour = {
-"data": kWhite,
-"dy": kGray,
-"dy_other": kGray,
-"dy_tautau": kGray+2,
-"wjets": kRed+1,
-"dibosons": kCyan,
-"singletop": kAzure,
-"s_top_eltau": kAzure,
-"s_top_lj": kAzure+1,
-"s_top_other": kAzure+2,
+# set data histo styles and
+# add data to the legend
+histo_data.SetMarkerStyle(21)
+leg.AddEntry(histo_data, "data", "e1 p")
 
-"tt_taultauh" : kCyan-3,
-"tt_jj": kGreen+4,
-"tt_lj": kGreen+3,
-"tt_em": kYellow-7,
-"tt_{em}": kYellow-7,
-"tt_ee": kAzure-9,
-"tt_mm": kGreen-9,
-"tt_eltau": kOrange+2,
-"tt_mutau": kOrange+1,
-"tt_{l\\tau-l}": kOrange+3,
-"tt_other": kCyan-5,
+# sum of MC to get the sum of errors
 
-"qcd": kViolet,
-}
-
-leg = TLegend(0.7, 0.7, 0.89, 0.89)
-
-used_histos = []
-
-for chan in list(f.GetListOfKeys()):
-    if chan.GetName() == 'weight_counter' or chan.GetName() == 'events_counter':
-        continue
-    if chan.GetName() != channel and chan.GetName() != args.shape:
-        continue
-
-    processes_keys = list(chan.ReadObj().GetListOfKeys())
-    sorted_pkeys = sorted(processes_keys, key=lambda pkey: pkey.GetName() if pkey.GetName() not in ('qcd') else 'z_' + pkey.GetName())
-    for process in sorted_pkeys:
-        nick = process.GetName()
-        logging.info(nick)
-
-        if nick == 'data':
-            continue
-
-        # if the channel with shapes for dy and wjets are given
-        # don't use other channels
-        if args.shape and nick in ('dy', 'wjets') and chan.GetName() != args.shape:
-            continue
-
-        for sys in list(process.ReadObj().GetListOfKeys()):
-            fixed_sys_name = sys_name
-            if 'TOPPT' in sys_name:
-                fixed_sys_name = sys_name if 'tt' in nick else 'NOMINAL'
-            if fixed_sys_name != sys.GetName():
-                continue
-
-            try:
-                for histo_key in list(sys.ReadObj().GetListOfKeys()):
-                    # rename for given shapes of dy and wjets
-                    if args.shape and nick in ('dy', 'wjets'):
-                        histo_name = '_'.join([args.shape, nick, fixed_sys_name, distr_name])
-                    else:
-                        histo_name = '_'.join([channel, nick, fixed_sys_name, distr_name])
-
-                    if histo_key.GetName() != histo_name:
-                        continue
-
-                    histo = histo_key.ReadObj()
-                    logging.info("%s   %s   %x = %f %f" % (histo_name, histo_key.GetName(), histo_key.GetName() == '_'.join([channel, nick, fixed_sys_name, distr_name]), histo.GetEntries(), histo.Integral()))
-
-                    col = nick_colour[nick]
-                    histo.SetFillColor( col );
-                    histo.SetMarkerStyle(20);
-                    histo.SetLineStyle(0);
-                    histo.SetMarkerColor(col);
-                    used_histos.append(histo) # hopefully root wont screw this up
-                    hs.Add(histo, "HIST")
-                    leg.AddEntry(histo, nick, "F")
-            except Exception as e:
-                print "failed", nick, e.__class__, e.__doc__
-
-# get the sum of histograms
+# loop through TList
 histos = hs.GetHists() # TList
 hs_sum1 = histos[0].Clone() #TH1F("sum","sum of histograms",100,-4,4);
 hs_sum1.SetName('mc_sum1')
@@ -152,10 +147,11 @@ hs_sum1.SetFillColor(1);
 hs_sum1.SetMarkerStyle(1)
 hs_sum1.SetMarkerColor(0)
 
-hs_sum2 = used_histos[0].Clone() #TH1F("sum","sum of histograms",100,-4,4);
+# loop through normal list
+hs_sum2 = used_histos[0][0].Clone() #TH1F("sum","sum of histograms",100,-4,4);
 hs_sum2.SetName('mc_sum2')
 
-for h in used_histos[1:]:
+for h, _ in used_histos[1:]:
     hs_sum2.Add(h)
 
 hs_sum2.SetFillStyle(3004);
@@ -163,14 +159,65 @@ hs_sum2.SetFillColor(1);
 hs_sum2.SetMarkerStyle(1)
 hs_sum2.SetMarkerColor(0)
 
-fout = TFile(args.mc_file.split('.root')[0] + '_%s_%s_%s.root' % (distr_name, channel, sys_name), 'RECREATE')
-fout.cd()
-histo_data.Write() #'data')
-hs.Write()
-hs_sum1.Write()
-hs_sum2.Write()
-leg.Write('leg')
-fout.Write()
-fout.Close()
+
+if not args.plot and not args.ratio:
+    fout = TFile(args.mc_file.split('.root')[0] + '_%s_%s_%s.root' % (distr_name, channel, sys_name), 'RECREATE')
+    fout.cd()
+    histo_data.Write() #'data')
+    for h in histos:
+        h.Write()
+    hs.Write()
+    hs_sum1.Write()
+    hs_sum2.Write()
+    leg.Write('leg')
+    fout.Write()
+    fout.Close()
+
+else:
+    from ROOT import gStyle, gROOT, TCanvas, TPad
+    gROOT.SetBatch()
+    gStyle.SetOptStat(0)
+    cst = TCanvas("cst","stacked hists",10,10,700,700)
+
+    if args.ratio and args.plot:
+        pad1 = TPad("pad1","This is pad1", 0., 0.3,  1., 1.)
+        pad2 = TPad("pad2","This is pad2", 0., 0.05,  1., 0.3)
+        pad1.Draw()
+        pad2.Draw()
+    elif args.ratio:
+        pad2 = TPad("pad2","This is pad2", 0., 0.05,  1., 1.)
+        pad2.Draw()
+    else:
+        pad1 = TPad("pad1","This is pad1", 0., 0.,  1., 1.)
+        pad1.Draw()
+
+    # plotting
+
+    if args.plot:
+        pad1.cd()
+
+        histo_data.Draw("e1 p")
+        hs.Draw("same")
+        hs_sum1.Draw("same e2")
+        histo_data.Draw("same e1p")
+        leg.Draw("same")
+
+    if args.ratio:
+        pad2.cd()
+
+        # calculate ratios
+        histo_data_relative = histo_data.Clone()
+        hs_sum1_relative = hs_sum1.Clone()
+        histo_data_relative.SetName("rel_data")
+        hs_sum1_relative.SetName("rel_mc")
+
+        histo_data_relative.Divide(hs_sum1)
+        hs_sum1_relative.Divide(hs_sum1)
+
+        hs_sum1_relative.Draw("e2")
+        histo_data_relative.Draw("e p same")
+
+    stack_or_ratio = ('_stack' if args.plot else '') + ('_ratio' if args.ratio else '')
+    cst.SaveAs('_'.join((args.mc_file.replace('/', ',').split('.root')[0], args.data_file.replace('/', ',').split('.root')[0], distr_name, channel, sys_name)) + stack_or_ratio + ".png")
 
 
