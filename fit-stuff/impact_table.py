d1af2627d9ed1ff63b5c00f3e8b379d4c6146143
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2018-04-26 19:24:20 +0100

    UP scripts
    normal names in the impact table
    styling in distr plots
    
    and help messages
    
    	modified:   fit-stuff/impact_table.py
    	modified:   jobsums/distr_ratios.py

--- fit-stuff/impact_table.py
+++ fit-stuff/impact_table.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.DEBUG)
 parser = argparse.ArgumentParser(
     formatter_class = argparse.RawDescriptionHelpFormatter,
     description = "print out the impacts in table format",
-    epilog = "Example:\npython impact_table.py latest_el_impacts.json"
+    epilog = "Example:\npython impact_table.py latest_el_impacts.json\npython impact_table.py latest_both_impacts.json --merge-pdfs"
     )
 
 parser.add_argument("impacts_file", help="json file with impacts")
@@ -27,7 +27,33 @@ impacts_js = (json.loads(f.read()))['params']
 
 impacts = {param['name']: param['impact_r'] for param in impacts_js}
 
-table_row_format = "\\lstinline!%10s!  & %4.2f \\\\"
+table_row_format = "\\lstinline!%25s!  & %4.2f \\\\"
+
+impact_name_dictionary = {
+"lumi_13TeV"  : "Luminosity",
+"tauID_eff"   : "Tau ID efficiency",
+"tau_fakes"   : "Tau mis-ID probability",
+"dy_norm"     : "DY normalization",
+"wjets_norm"  : "W+jets normalization",
+"qcd_norm"    : "QCD normalization",
+"bSF"         : "b-tagging efficiency",
+"LEP"         : "lepton ID efficiency",
+"JES"         : "Jet Energy Scale",
+"JER"         : "Jet Energy Resolution",
+"TauES"       : "Tau Energy Scale",
+"PU"          : "Pile-Up uncertainty",
+"TOPPT"       : "Top $p_{T}$",
+"Frag"        : "b fragmentation",
+"Peterson"    : "b frag., Peterson variation",
+"SemilepBR"   : "b decay tables, Semileptonic BR",
+"Mr"          : "ME Renormalization scale",
+"Mf"          : "ME Factorization scale",
+"Mfr"         : "ME combined scale variation",
+"FSR" : "Final State Radiation",
+"ISR" : "Initial State Radiation",
+"HDAMP" : "ME-PS matching (hdamp)",
+"TuneCUETP8M2T4" : "Underlying event (Pythia tune)",
+}
 
 if not args.merge_pdfs:
     for name, imp in sorted(impacts.items(), key=lambda k: k[1], reverse=True):
@@ -35,10 +61,11 @@ if not args.merge_pdfs:
 
 else:
     pdf_quadrature_sum = 0.
-    for name, imp in sorted(impacts.items(), key=lambda k: k[1], reverse=True):
-        if 'PDF' in name:
+    for nick, imp in sorted(impacts.items(), key=lambda k: k[1], reverse=True):
+        if 'PDF' in nick or 'AlphaS' in nick:
             pdf_quadrature_sum += imp**2
             continue
+        name = impact_name_dictionary.get(nick, nick)
         print table_row_format % (name, imp*100)
     pdf_impact = pdf_quadrature_sum**0.5
     print table_row_format % ('PDF', pdf_impact*100)
--- jobsums/distr_ratios.py
+++ jobsums/distr_ratios.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.DEBUG)
 parser = argparse.ArgumentParser(
     formatter_class = argparse.RawDescriptionHelpFormatter,
     description = "plot data and different SYS of MC sums w.r. to NOMINAL MC",
-    #epilog = "Example:\n$ python job_submit.py ttbarleps80_eventSelection jobing/my_runAnalysis_cfg_NEWSUBMIT.templ.py bin/ttbar-leptons-80X/analysis/dsets_testing_noHLT_TTbar.json test/tests/outdir_test_v11_ttbar_v8.40/"
+    epilog = "Example:\npython distr_ratios.py mc_v25p2_v26p2.root -c ctr_old_mu_sel:Mt_lep_met_f -p tt_lj --ratio-channel-distrs ctr_old_mu_sel:Mt_lep_met_t2,ctr_old_mu_sel:Mt_lep_met_t3,ctr_old_mu_sel:Mt_lep_met_t4,ctr_old_mu_sel:Mt_lep_met_t5,ctr_old_mu_sel:Mt_lep_met_t6"
     )
 
 parser.add_argument("data_file", help="data file name")
@@ -32,11 +32,27 @@ parser.add_argument("--ratio-range", type=float, default=0.5, help="range of rat
 #parser.add_argument("--y-max",       type=float,              help="set the maximum on Y axis")
 parser.add_argument("--y-range",     type=str,              help="set Y range as `ymin,ymax`")
 
+parser.add_argument("--title",   type=str, default=None, help="title for the plot")
+parser.add_argument("--title-x", type=str, default=None, help="title for the plot")
+
 parser.add_argument("--add-y-line", type=float, help="add horizontal line at the given Y value")
 
 args = parser.parse_args()
 
 assert isfile(args.data_file)
+
+if args.title is not None:
+    title = args.title
+else:
+    title = args.process
+
+
+ref_chan, ref_distr = args.channel_distr.split(':')
+
+if args.title_x is not None:
+    title_x = args.title_x
+else:
+    title_x = ref_distr
 
 logging.info("import ROOT")
 
@@ -51,7 +67,9 @@ fdata = TFile(args.data_file)
 
 leg = TLegend(0.6,0.7, 0.9,0.9)
 
-distr_nick = {'Mt_lep_met_t1': 'pt < 30',
+distr_nick = {
+    'Mt_lep_met_f':  'inclusive',
+    'Mt_lep_met_t1': 'pt < 30',
     'Mt_lep_met_t2': 'pt < 40',
     'Mt_lep_met_t3': 'pt < 50',
     'Mt_lep_met_t4': 'pt < 70',
@@ -61,7 +79,6 @@ distr_nick = {'Mt_lep_met_t1': 'pt < 30',
 
 # get nominal MC
 sys = args.systematic
-ref_chan, ref_distr = args.channel_distr.split(':')
 
 nom_MC = fdata.Get(ref_chan + '/{proc}/{sys}/{chan}_{proc}_{sys}_{distr}'.format(proc=args.process, chan=ref_chan, sys=args.systematic, distr=ref_distr))
 nom_MC.Print()
@@ -122,11 +139,13 @@ if args.ratio:
     data.GetXaxis().SetLabelOffset(999)
     data.GetXaxis().SetLabelSize(0)
 
+print 'title', title
+
+nom_MC.SetTitle(title)
+nom_MC.SetXTitle(title_x)
+
 nom_MC.Divide(nom_MC)
 nom_MC.Print()
-
-nom_MC.SetTitle(args.process)
-nom_MC.SetXTitle(ref_distr)
 
 if args.y_range:
     y_min, y_max = [float(x) for x in args.y_range.split(',')]
@@ -145,10 +164,14 @@ if args.logy:
     #pad1.SetLogy()
     gPad.SetLogy()
 
+nom_MC.SetTitle(title)
+nom_MC.SetLineWidth(2)
 nom_MC.Draw("e")
 
 for histo in histo_MCs:
     histo.Print()
+    histo.SetTitle(title)
+    histo.SetLineWidth(2)
     histo.Draw("same e")
 
 if args.add_y_line:
