8f83003ae9005354217528c726080dc4aefb5719
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2018-05-06 17:52:27 +0100

    Back opt elmu channels
    and new distrs for met/lep phi stuff
    
    	modified:   support_channel_distrs.py

--- support_channel_distrs.py
+++ support_channel_distrs.py
@@ -737,6 +737,9 @@ def transverse_mass_pts(v1_x, v1_y, v2_x, v2_y):
     v1v2 = TMath.Sqrt((v1_x*v1_x + v1_y*v1_y)*(v2_x*v2_x + v2_y*v2_y))
     return TMath.Sqrt(2*(v1v2 - (v1_x*v2_x + v1_y*v2_y)))
 
+def transverse_cos(v1, v2):
+    met_lep_cos = (v1.Px()*v2.Px() + v1.Py()*v2.Py()) / TMath.Sqrt((v1.Px()*v1.Px() + v1.Py()*v1.Py())* (v2.Px()*v2.Px() + v2.Py()*v2.Py()))
+    return met_lep_cos
 
 def calc_lj_var(ev, light_jets, b_jets, save_all_permutations=False, isMC=False):
     closest_pair_gens = (0, 0)
@@ -1938,6 +1941,9 @@ def full_loop(tree, dtag, lumi_bcdef, lumi_gh, logger, channels_to_select):
              'corr_met':           TH1D('%s_%s_%s_corr_met'   % (chan, proc, sys), '', 30, 0, 300),
              'met_objects':        TH1D('%s_%s_%s_met_objects'% (chan, proc, sys), '', 30, 0, 300),
              'met_lep_phi':        TH1D('%s_%s_%s_met_lep_phi'% (chan, proc, sys), '', 30, -3.2, 3.2),
+             'met_lep_cos':        TH1D('%s_%s_%s_met_lep_cos'% (chan, proc, sys), '', 44, -1.1, 1.1),
+             'allobj_lep_cos':     TH1D('%s_%s_%s_allobj_lep_cos'    % (chan, proc, sys), '', 44, -1.1, 1.1),
+             'Mt_lep_allobj_met':  TH1D('%s_%s_%s_Mt_lep_allobj_met' % (chan, proc, sys), '', 20, 0, 250),
              'lep1_phi':           TH1D('%s_%s_%s_lep1_phi'   % (chan, proc, sys), '', 30, -3.2, 3.2),
              'lep2_phi':           TH1D('%s_%s_%s_lep2_phi'   % (chan, proc, sys), '', 30, -3.2, 3.2), # tau in lep-tau channels
              'met_phi':            TH1D('%s_%s_%s_met_phi'    % (chan, proc, sys), '', 30, -3.2, 3.2), # tau in lep-tau channels
@@ -2299,7 +2305,7 @@ def full_loop(tree, dtag, lumi_bcdef, lumi_gh, logger, channels_to_select):
         # OPTIMIZATION tests are done only on pass_mu
         #passes_optimized = pass_mu_all or pass_el_all or pass_mumu or pass_elmu
         passes_optimized = pass_mu or pass_el or pass_mumu or pass_elmu or pass_mu_all or pass_el_all or pass_elel
-        event_passes = pass_mu or pass_el # pass_elmu or pass_elmu_el # passes_optimized #
+        event_passes = pass_elmu or pass_elmu_el # pass_mu or pass_el # passes_optimized #
 
         if not event_passes: continue
         control_counters.Fill(51)
@@ -4912,6 +4918,19 @@ def full_loop(tree, dtag, lumi_bcdef, lumi_gh, logger, channels_to_select):
                 out_hs[(chan, record_proc, sys_name)]['met_objects']  .Fill(all_sel_objects.pt(), record_weight)
                 out_hs[(chan, record_proc, sys_name)]['met_cancelation']  .Fill(met_cancelation, record_weight)
                 out_hs[(chan, record_proc, sys_name)]['met_lep_phi']  .Fill((proc_met - lep_p4[0]).Phi(), record_weight)
+
+                met_lep_cos = transverse_cos(proc_met, lep_p4[0])
+                all_obj_met = -all_sel_objects
+                allobj_lep_cos = transverse_cos(all_obj_met, lep_p4[0])
+                out_hs[(chan, record_proc, sys_name)]['met_lep_cos']     .Fill(met_lep_cos, record_weight)
+                out_hs[(chan, record_proc, sys_name)]['allobj_lep_cos']  .Fill(allobj_lep_cos, record_weight)
+                Mt_lep_allobj_met      = transverse_mass_pts(lep_p4[0].Px(), lep_p4[0].Py(), all_obj_met.Px(), all_obj_met.Py())
+                out_hs[(chan, record_proc, sys_name)]['Mt_lep_allobj_met']   .Fill(Mt_lep_allobj_met,     record_weight)
+
+                # 2D distrs
+                out_hs[(chan, record_proc, sys_name)]['met_lep_phis']    .Fill(proc_met.Phi(), lep_p4[0].Phi(), record_weight)
+                out_hs[(chan, record_proc, sys_name)]['met_allobj_phis'] .Fill(proc_met.Phi(), all_obj_met.Phi(), record_weight)
+                out_hs[(chan, record_proc, sys_name)]['met_allobj_pts']  .Fill(proc_met.pt(),  all_obj_met.pt(), record_weight)
 
                 out_hs[(chan, record_proc, sys_name)]['lep1_phi']      .Fill(lep_p4[0].phi(), record_weight)
                 out_hs[(chan, record_proc, sys_name)]['met_phi']       .Fill(proc_met.phi(), record_weight)
