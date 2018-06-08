e670b501788f84f1bd4df84d6a9cc8e47e7ebee9
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2018-06-08 19:18:04 +0100

    UP propper script for rate yields in systematics
    	modified:   yield_variations.py

--- proc/jobsums/yield_variations.py
+++ proc/jobsums/yield_variations.py
@@ -6,15 +6,17 @@ parser = argparse.ArgumentParser(
     formatter_class = argparse.RawDescriptionHelpFormatter,
     description = "find the systematic variations of the yield in a channel for given process",
     epilog = """Example:
-python yield_variations.py mc_v25v26pR5_updowns_with_data.root -c ctr_old_mu_sel_lj -p tt_taultauh,tt_lj,tt_other,tt_mutau,tt_taulj -s FSRUp,FSRDown,ISRUp,ISRDown,HDAMPUp,HDAMPDown,TuneCUETP8M2T4Up,TuneCUETP8M2T4Down"""
+python yield_variations.py mc_v25v26pR5_updowns_with_data.root -c ctr_old_mu_sel_lj -p tt_taultauh,tt_lj,tt_other,tt_mutau,tt_taulj -s FSR,ISR,HDAMP,TuneCUETP8M2T4"""
     )
 
 parser.add_argument("data_file",    help="full Data+MC file name")
-parser.add_argument("-c", "--channel",   type=str, help="the channel to look in")
+parser.add_argument("-c", "--channels",  type=str, help="the channels to look in")
 parser.add_argument("-p", "--processes", type=str, default='tt_taultauh,tt_lj,tt_other,tt_mutau,tt_taulj', help="processes to consider")
 parser.add_argument("-d", "--distr",     type=str, default='yield', help="the distribution (yield by default)")
-parser.add_argument("-s", "--sys",       type=str, default='FSRUp,FSRDown,ISRUp,ISRDown,HDAMPUp,HDAMPDown,TuneCUETP8M2T4Up,TuneCUETP8M2T4Down', help="systematics to consider")
-parser.add_argument("--debug",     action = "store_true", help="log debug output")
+parser.add_argument("-s", "--sys",       type=str, default='FSR,ISR,HDAMP,TuneCUETP8M2T4', help="systematics to consider")
+
+parser.add_argument("--debug",      action = "store_true", help="log debug output")
+parser.add_argument("--to-average", action = "store_true", help="ratio to average of systematics, instead of the nominal tt")
 
 
 args = parser.parse_args()
@@ -36,10 +38,47 @@ assert isfile(args.data_file)
 
 fdata = TFile(args.data_file)
 
-for process in args.processes.split(','):
-    nominal = fdata.Get("{chan}/{proc}/NOMINAL/{chan}_{proc}_NOMINAL_{distr}".format(chan=args.channel, proc=process, distr=args.distr))
-    print process
-    for sys in args.sys.split(','):
-        systematic = fdata.Get("{chan}/{proc}/{sys}/{chan}_{proc}_{sys}_{distr}".format(chan=args.channel, proc=process, sys=sys, distr=args.distr))
-        print sys, systematic.Integral() / nominal.Integral()
+channels  = []
+processes = []
+for channel in args.channels.split(','):
+    for process in args.processes.split(','):
+        channels.append(channel)
+        processes.append(process)
+print ' '*20 + '  ' + '  '.join(('%20s' % chan for chan in channels))
+print ' '*20 + '  ' + '  '.join(('%20s' % proc for proc in processes))
+
+for sys in args.sys.split(','):
+    sys_variations = []
+    for channel in args.channels.split(','):
+        for process in args.processes.split(','):
+            systematic_up   = fdata.Get("{chan}/{proc}/{sys}/{chan}_{proc}_{sys}_{distr}".format(chan=channel, proc=process, sys=sys+'Up',   distr=args.distr))
+            systematic_down = fdata.Get("{chan}/{proc}/{sys}/{chan}_{proc}_{sys}_{distr}".format(chan=channel, proc=process, sys=sys+'Down', distr=args.distr))
+            if not systematic_up:
+                print "no " + sys + 'Up'
+                sys_up_yield   = 0.
+            else:
+                sys_up_yield   = systematic_up.Integral()
+
+            if not systematic_down:
+                print "no " + sys + 'Down'
+                sys_down_yield = 0.
+            else:
+                sys_down_yield = systematic_down.Integral()
+
+            if args.to_average:
+                reference = (sys_up_yield + sys_down_yield) * 0.5
+            else:
+                nominal = fdata.Get("{chan}/{proc}/NOMINAL/{chan}_{proc}_NOMINAL_{distr}".format(chan=channel, proc=process, distr=args.distr))
+                reference = nominal.Integral()
+
+            logging.debug('%s %s %s %f %f %f' % (channel, process, sys, sys_up_yield, sys_down_yield, reference))
+            if reference == 0:
+                logging.warning('reference == 0 in %s %s %s' % (channel, process, sys))
+                continue
+
+            var_up   = sys_up_yield   / reference
+            var_down = sys_down_yield / reference
+            sys_variations.append((var_up, var_down))
+
+    print ('%20s  ' % sys) + '  '.join(('%20s' % ('%f/%f' % (var_up, var_down)) for var_up, var_down in sys_variations))
 
