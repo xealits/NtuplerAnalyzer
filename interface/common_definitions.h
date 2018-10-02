1760a4c69b1d67b2dbf7ea7e8e8afc815e122219
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2018-10-02 17:30:59 +0100

    Forgot new file
    	new file:   interface/common_definitions.h

---
+++ interface/common_definitions.h
@@ -0,0 +1,8 @@
+#ifndef COMMONDEFINITIONS_H
+#define COMMONDEFINITIONS_H
+
+typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > LorentzVector;
+
+
+#endif /* COMMONDEFINITIONS_Httbar-leptons-80X */
+
