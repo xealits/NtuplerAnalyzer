58d6555b9447724ba8cfd226a79879dc21ed86f1
Oleksii Toldaiev <alex.toldaiev@cern.ch>
2018-10-09 16:01:03 +0200

    Fixed the invisible part of gen taus
    tested orig = vis + invis (X, Y, Z components and even T -- so it cannot be some rotation of something)
    it mostly hols, but some very small amount of events is within 1+-0.004
    -- will figure this out later, for now starting the full run (for preliminary tt-dy test and corresponding studies)
    
    	modified:   src/ProcessingGenParticles.cc

--- src/ProcessingGenParticles.cc
+++ src/ProcessingGenParticles.cc
@@ -309,22 +309,34 @@ if (save_only_visible && !(pdgId == 12 || pdgId == 14 || pdgId == 16))
 		}
 	}
 
-if (!save_only_visible && (pdgId == 12 || pdgId == 14 || pdgId == 16))
+if (!save_only_visible)
 	{
-	// it's a neutrino, final state, check if it is not saved and sum it
-	LorentzVector part_p4 = part->p4();
-	//if (std::find(saved_particles.begin(), saved_particles.end(), part) == saved_particles.end())
-	bool dR_match_to_saved = false;
-	for (int saved_p=0; saved_p<saved_particles.size(); saved_p++)
+	if (pdgId == 12 || pdgId == 14 || pdgId == 16)
 		{
-		dR_match_to_saved = reco::deltaR(part_p4, saved_particles[saved_p]) < 0.0001;
-		if (dR_match_to_saved) break;
+		// it's a neutrino, final state, check if it is not saved and sum it
+		LorentzVector part_p4 = part->p4();
+		//if (std::find(saved_particles.begin(), saved_particles.end(), part) == saved_particles.end())
+		bool dR_match_to_saved = false;
+		for (int saved_p=0; saved_p<saved_particles.size(); saved_p++)
+			{
+			dR_match_to_saved = reco::deltaR(part_p4, saved_particles[saved_p]) < 0.0001;
+			if (dR_match_to_saved) break;
+			}
+		// then save it:
+		if (! dR_match_to_saved)
+			{
+			sum_parts += part_p4;
+			saved_particles.push_back(part_p4);
+			}
 		}
-	// then save it:
-	if (! dR_match_to_saved)
+	else
 		{
-		sum_parts += part_p4;
-		saved_particles.push_back(part_p4);
+		// loop through daughters
+		for (int d_i=0; d_i < part->numberOfDaughters(); d_i++)
+			{
+			const reco::Candidate * daughter = part->daughter(d_i);
+			sum_final_cands(daughter, saved_particles, sum_parts, save_only_visible);
+			}
 		}
 	}
 
