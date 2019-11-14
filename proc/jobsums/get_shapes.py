61b96f53584b05e4ebb883f172e02e1071eb7d1f
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2019-11-14 18:55:19 +0000

    All the post-FR plotting stuff
    	modified:   ../../bin/BuildFile.xml
    	modified:   get_shapes.py
    	modified:   plotting_root.py

--- proc/jobsums/get_shapes.py
+++ proc/jobsums/get_shapes.py
@@ -7,6 +7,7 @@ import pdb
 from draw_overflows import DrawOverflow
 
 
+
 parser = argparse.ArgumentParser(
     formatter_class = argparse.RawDescriptionHelpFormatter,
     description = "to compare shapes from different files",
@@ -27,11 +28,16 @@ parser.add_argument("--x-title",     type=str,      help="title of X axis")
 parser.add_argument("--y-title",     type=str,      help="title of Y axis")
 parser.add_argument("--fonts1",      type=int, default=43, help="axis font")
 parser.add_argument("--fonts1-size", type=int, default=25, help="axis and legend font size")
+parser.add_argument("--font-size-axes-titles", type=int, default=40, help="axis and legend font size")
 parser.add_argument("--title",       type=str,      help="title of the pad")
 parser.add_argument('--left-title',  type=str, default="CMS #font[52]{Simulation}", help="the text of the left label title")
 parser.add_argument('--left-title-inside',  action='store_true', help="put the left label inside the figure box")
 
 parser.add_argument("--draw-overflows", type=float, help="draw the bin with overflows in the given width")
+
+parser.add_argument("--vertical-line",  type=str,   help="format X,Ymin,Ymax draw a line from Ymin to Ymax at the given X")
+
+parser.add_argument("--add-outlier-plot",  type=str,   help="format S,B with Signal and Background outlier events")
 
 
 parser.add_argument("--outname",     type=str,      help="set the output filename")
@@ -91,7 +97,10 @@ def new_color():
 colors = {'red': kRed, 'black': kBlack, 'blue': kBlue}
 styles = {}
 
-leg = TLegend(0.5, 0.55, 0.89, 0.89)
+if args.left_title_inside:
+    leg = TLegend(0.6, 0.55, 0.89, 0.89)
+else:
+    leg = TLegend(0.7, 0.55, 0.99, 0.89)
 leg.SetBorderSize(0)
 leg.SetTextFont(args.fonts1)
 leg.SetTextSize(args.fonts1_size)
@@ -169,6 +178,9 @@ for i, fileparameter in enumerate(args.input_files):
 
     histo.Sumw2()
 
+    if args.title:
+        histo.SetTitle(args.title)
+
     logging.debug("histo    : %20s" % nick)
     logging.debug("integral : %f" % histo.Integral())
     histo.SetDirectory(0)
@@ -193,18 +205,50 @@ for i, fileparameter in enumerate(args.input_files):
 
 cst = TCanvas("cst","stacked hists",10,10,700,700)
 
-pad1 = TPad("pad1","This is pad1", 0., 0.,  1., 1.)
+y_axis_tick_len = 0.03
+bottom_margin = 0.17
 
-pad1.Draw()
+if args.add_outlier_plot:
+    padsplit_x = 0.9
+    pad1 = TPad("pad1","This is pad1", 0.,  0., padsplit_x, 1.)
+    pad2 = TPad("pad2","This is pad2", padsplit_x, 0., 1.,  1.)
+    pad1.SetLeftMargin(0.14)
+    pad1.SetRightMargin(0.)
+    pad1.SetBottomMargin(bottom_margin)
+    pad2.SetTitle("")
+    pad2.SetLeftMargin(0.)
+    pad2.SetBottomMargin(bottom_margin)
+    pad1.Draw()
+    pad2.Draw()
+
+else:
+    padsplit_x = 1.
+    pad1 = TPad("pad1","This is pad1", 0., 0.,  1., 1.)
+    pad1.SetLeftMargin(0.14)
+    pad1.SetRightMargin(0.01)
+    pad1.SetBottomMargin(bottom_margin)
+    pad1.Draw()
+
+if args.title:
+    pad1.SetTitle(args.title)
+
 
 pad1.SetTicks()
-
 if args.logy:
     pad1.SetLogy()
+
+if args.add_outlier_plot:
+    pad2.SetTicks()
+    #pad1.SetTicks(1, 0) 
+    #pad2.SetTicks(0, 1)
+    if args.logy:
+        pad2.SetLogy()
 
 pad1.cd()
 
 plots_to_legend = []
+
+drawn = False
 
 if not args.formula:
     for i, (nick, h) in enumerate(histos.items()):
@@ -228,7 +272,6 @@ if not args.formula:
 else:
     # parse the formula
     import re
-    drawn = False
     form_histos = []
 
     # a sub-form is a distribution to plot
@@ -278,6 +321,10 @@ else:
             # construct the histo of the subform
             factor, nick = nick_pairs[0]
             histo = histos[nick].Clone()
+
+            if args.title:
+                histo.SetTitle(args.title)
+
             formula_name = "formula_%d_%d" % (form_i, sum_i)
             histo.SetName(formula_name)
             histo.SetDirectory(0)
@@ -328,6 +375,8 @@ else:
 
     # and draw
     for histo in form_histos:
+        if args.title:
+            histo.SetTitle(args.title)
         # normalize bin width if needed
         if args.norm_binwidth:
             #
@@ -347,6 +396,8 @@ else:
 
 
         if drawn:
+            if args.title:
+                histo.SetTitle(args.title)
             if y_max is not None and y_min is not None:
                 logging.debug("setting min-max")
                 histo.SetMaximum(y_max)
@@ -370,25 +421,38 @@ else:
 
             histo.GetYaxis().SetLabelFont(args.fonts1)
             histo.GetXaxis().SetLabelFont(args.fonts1)
-            histo.GetYaxis().SetLabelSize(args.fonts1_size*0.75)
-            histo.GetXaxis().SetLabelSize(args.fonts1_size*0.75)
+            #histo.GetYaxis().SetLabelSize(args.fonts1_size*0.75)
+            #histo.GetXaxis().SetLabelSize(args.fonts1_size*0.75)
+            histo.GetYaxis().SetLabelSize(args.fonts1_size)
+            histo.GetXaxis().SetLabelSize(args.fonts1_size)
+            histo.GetYaxis().SetTitleOffset(1.15)
 
             histo.GetYaxis().SetTitleFont(args.fonts1)
             histo.GetXaxis().SetTitleFont(args.fonts1)
-            histo.GetYaxis().SetTitleSize(args.fonts1_size)
-            histo.GetXaxis().SetTitleSize(args.fonts1_size)
+            histo.GetYaxis().SetTitleSize(args.font_size_axes_titles)
+            histo.GetXaxis().SetTitleSize(args.font_size_axes_titles)
             if y_max is not None and y_min is not None:
                 logging.debug("setting min-max")
                 histo.SetMaximum(y_max)
                 histo.SetMinimum(y_min)
 
+            # set the length of ticks on Y axis
+            # it is done consistently with the possible additional plot
+            histo.GetYaxis().SetTickLength(y_axis_tick_len/padsplit_x)
             # draw the first one with line
             histo.SetFillColor(0)
-            if args.draw_error_bars:
-                histo.Draw("hist e")
-            else:
-                histo.Draw("hist")
+            histo.Draw("hist e" if args.draw_error_bars else "hist")
             drawn = True
+
+            # draw the verticle line in the background
+            if args.vertical_line:
+                x, ymin, ymax = (float(i) for i in args.vertical_line.split(','))
+                l = TLine(x, ymin, x, ymax)
+                l.Draw("same")
+
+                #drawn = True
+                #histo.Draw("hist same")
+                histo.Draw("hist e same" if args.draw_error_bars else "hist same")
 
 for histo, subform in reversed(plots_to_legend):
     if subform.strip() in legend_names:
@@ -396,12 +460,64 @@ for histo, subform in reversed(plots_to_legend):
     else:
         leg.AddEntry(histo, subform) #, "l")
 
+
+if not args.no_legend:
+    leg.Draw("same")
+
+if args.add_outlier_plot:
+    sig, bkg = (float(i) for i in args.add_outlier_plot.split(','))
+    pad2.cd()
+
+    #l = TLine(0., 0.5, 1., 0.5)
+    #l.Draw("same")
+
+    sig_hist = TH1D("untagged_events_signal", "", 1, 0., 1.)
+    sig_hist.SetBinContent(1, sig)
+    sig_hist.SetMaximum(y_max)
+    sig_hist.SetMinimum(y_min)
+    sig_hist.SetLineColor(colors['red'])
+    sig_hist.SetLineWidth(3)
+
+    # remove ticks
+    sig_hist.GetXaxis().SetTickLength(0.)
+
+    # Set the correct length of Y axis ticks
+    sig_hist.GetYaxis().SetTickLength(y_axis_tick_len/(1.-padsplit_x))
+
+    ## remove the labels on ticks
+    #sig_hist.GetXaxis().SetLabelOffset(999)
+    #sig_hist.GetXaxis().SetLabelSize(0)
+
+    # set "untagged" as a label
+    #sig_hist.GetXaxis().SetLabelOffset(999)
+    sig_hist.GetXaxis().SetLabelFont(args.fonts1)
+    sig_hist.GetXaxis().SetLabelSize(args.fonts1_size) # does not fit
+    #sig_hist.GetXaxis().SetLabelSize(25)
+
+    # set the title "untagged"
+    #sig_hist.GetXaxis().SetTitle("untagged")
+    # set alphanumeric label
+    sig_hist.GetXaxis().SetBinLabel(1, "untagged")
+    sig_hist.LabelsOption("v")
+
+    sig_hist.Draw("same")
+
+    bkg_hist = TH1D("untagged_events_signal", "", 1, 0., 1.)
+    bkg_hist.SetBinContent(1, bkg)
+    bkg_hist.SetLineColor(colors['black'])
+    bkg_hist.SetLineWidth(3)
+    bkg_hist.SetLineStyle(2)
+    bkg_hist.Draw("same")
+
+
+cst.cd()
+
 if args.left_title:
     #left_title = TPaveText(0.12, 0.8, 0.36, 0.88, "brNDC")
     if args.left_title_inside:
-        left_title = TPaveText(0.14, 0.82, 0.7, 0.87, "brNDC")
+        left_title = TPaveText(0.19, 0.82, 0.5, 0.87, "brNDC")
     else:
-        left_title = TPaveText(0.1, 0.92, 0.7, 0.97, "brNDC")
+        left_title = TPaveText(0.15,  0.92, 0.5, 0.97, "brNDC")
     #left_title.SetBorderSize(1)
     #left_title.SetTextAlign(11) # this adjust to left (+ margin inside the box and top)
     left_title.SetTextAlign(13) # this adjust to left (+ margin inside the box and top)
@@ -420,7 +536,9 @@ if args.left_title:
     left_title .Draw("same")
 
 #right_title = TPaveText(0.65, 0.91, 0.89, 0.96, "brNDC")
-right_title = TPaveText(0.65, 0.92, 0.9, 0.97, "brNDC")
+#right_title = TPaveText(0.65, 0.92, 0.9 + (1 - padsplit_x), 0.97, "brNDC")
+#right_title = TPaveText(0.65, 0.92, 0.89 + (1 - padsplit_x), 0.97, "brNDC")
+right_title = TPaveText(0.65, 0.92, 0.99, 0.97, "brNDC")
 right_title.SetTextAlign(33)
 right_title.SetMargin(0)
 right_title.AddText("(13 TeV)")
@@ -429,9 +547,6 @@ right_title.SetFillColor(0)
 
 right_title.Draw("same")
 
-
-if not args.no_legend:
-    leg.Draw("same")
 
 if args.outname:
     cst.SaveAs(args.outname)
